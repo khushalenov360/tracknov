0cb6f9c6c7733bf46c98ffdf03d72dda19c18ee0
khushal <khush@enov360.com>
2026-05-30 22:51:20 +0530

    chore: update file_index with URLs

--- harita/update_file_index.ps1
+++ harita/update_file_index.ps1
@@ -1,4 +1,8 @@
 $RepoRoot = Resolve-Path ".."
 $ExcludePattern = '\\node_modules\\|\\.git\\|\\.next\\'
-Get-ChildItem -Path $RepoRoot -Recurse -File | Where-Object { $_.FullName -notmatch $ExcludePattern } | ForEach-Object { "- $($_.FullName.Replace($RepoRoot.Path + '\', '').Replace('\', '/'))" } | Out-File -FilePath "$PSScriptRoot\file_index.md" -Encoding utf8
+Get-ChildItem -Path $RepoRoot -Recurse -File | Where-Object { $_.FullName -notmatch $ExcludePattern } | ForEach-Object {
+    $relPath = $_.FullName.Replace($RepoRoot.Path + '\', '').Replace('\', '/')
+    $urlPath = $relPath.Replace(' ', '%20')
+    "- [$relPath](https://github.com/khushalenov360/tracknov/blob/main/$urlPath)"
+} | Out-File -FilePath "$PSScriptRoot\file_index.md" -Encoding utf8
 Write-Host "file_index.md has been successfully updated in the harita folder."
