6cf746e8867e4427f2f8f97b8c5ee80145184063
khushal <khush@enov360.com>
2026-05-30 14:56:57 +0530

    feat: implement 04_API_CATALOG boundary and move file_index to harita

--- update-file-index.ps1
+++
@@ -1,3 +0,0 @@
-$ExcludePattern = '\\node_modules\\|\\.git\\|\\.next\\'
-Get-ChildItem -Recurse -File | Where-Object { $_.FullName -notmatch $ExcludePattern } | ForEach-Object { "- $($_.FullName.Replace((Get-Location).Path + '\', '').Replace('\', '/'))" } | Out-File -FilePath "file index.md" -Encoding utf8
-Write-Host "file index.md has been successfully updated."
